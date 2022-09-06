1fb769bdd94a164a587684af6501648b50eed314
Tim Albert <timothy.m.albert@gmail.com>
2022-09-06 15:01:57 -0400

    Plane updates

--- source/_layouts/update.blade.php
+++ source/_layouts/update.blade.php
@@ -1,7 +1,7 @@
 @extends('_layouts.main')
 
 @section('body')
-      <div class="max-w-2xl mx-auto px-6 pt-4">
+      <div class="max-w-2xl mx-auto px-6 py-4">
             <div class="text-xl font-light leading-relaxed">
                 <h1 class="text-2xl sm:text-3xl text-center font-bold uppercase">
                     {{ $page->title }}  <span class="text-yellow-500">&bull;</span>  {{ $page->date }}
@@ -17,9 +17,23 @@
                     @yield('content')
                 </div>
 
+                @if($page->readings)
+                <h2 class="font-semibold text-2xl">Some things I've enjoyed this trip</h2>
+                <ul class="mt-8">
+                    @foreach($page->readings as $reading)
+                        <li class="block w-full mt-2">
+                            <a class="flex hover:text-yellow-500" href="{{ $reading[3] }}">
+                                @include('_icons.'.$reading[0])
+                                <p class="ml-4 font-mono text-base">{{ $reading[1] }} - {{ $reading[2] }}</p>
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+                @endif
+
                 @if ($page->getNext())
                     <div class="mt-12 pt-3 border-t border-yellow-500">
-                        <a class="font-mono hover:text-yellow-500 flex justify-center" href="{{ $page->getNext()->getPath() }}">
+                        <a class="font-mono text-base hover:text-yellow-500 flex justify-center" href="{{ $page->getNext()->getPath() }}">
                             <p class="ml-4">{{ $page->getNext()->title }}</p>
                         </a>
                     </div>
---
+++ source/_updates/2022-09-06-hope-through-terminal-illness.blade.md
@@ -0,0 +1,9 @@
+---
+extends: _layouts.update
+section: content
+title: Hope Through Terminal Illness
+date: 9/6/22
+image: boys-at-beach.jpg
+---
+
+Suffering has always been a comfortable distance.
