3a8f0a585b07cca7bd280c9fd46b13f9bb19ae8a
Tim Albert <timothy.m.albert@gmail.com>
2023-09-30 21:34:54 -0400

    fix card

--- source/_partials/card.blade.php
+++ source/_partials/card.blade.php
@@ -2,7 +2,7 @@
     <div class="w-full min-h-80 aspect-w-1 aspect-h-1 rounded-t-md overflow-hidden group-hover:opacity-75 lg:h-50 lg:aspect-none">
         <img src="/assets/images/{{ $card->image }}" alt="" class="w-full h-full object-center object-cover lg:w-full lg:h-full">
     </div>
-    <div class="lg:flex lg:justify-between lg:items-center p-4 pb-5">
+    <div class="flex justify-between items-center p-4 pb-5">
         <h3 class="text-lg leading-tight text-slate-100 font-bold leading-normal">
             <a href="{{ $card->getUrl() }}">
                 <span aria-hidden="true" class="absolute inset-0"></span>
