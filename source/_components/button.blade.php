25bc6a21e5dcc7c4cacb354c87c376be2fd22080
Tim Albert <timothy.m.albert@gmail.com>
2023-10-08 22:38:05 -0400

    edit intro

--- source/_components/button.blade.php
+++ source/_components/button.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ $href ?? '#' }}" class="align-center items-center bg-slate-100/20 hover:bg-slate-500 uppercase text-sm rounded-full px-4 py-1.5 lg:py-2 {{ $btnclass ?? '' }} {{ isset($icon) ? 'flex' : '' }}">
+<a href="{{ $href ?? '#' }}" class="align-center items-center text-slate-100 bg-slate-50/5 hover:bg-slate-950/10 border border-slate-100/20 hover:bg-slate-500 uppercase text-sm rounded-full px-4 py-1.5 lg:py-2 {{ $btnclass ?? '' }} {{ isset($icon) ? 'flex' : '' }}">
     @if(isset($icon))
         <span class="mr-2 text-yellow-500">
             @include('_icons.'.$icon)
