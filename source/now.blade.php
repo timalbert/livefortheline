788c30e6f9578c24db32d666c8586a3c4bb44452
Tim Albert <timothy.m.albert@gmail.com>
2024-01-28 09:21:00 -0500

    fix now

--- source/now.blade.php
+++ source/now.blade.php
@@ -4,7 +4,7 @@ items: [
     ["book", "Dark Clouds, Deep Mercy: Discovering the Grace of Lament", "Mark Vroegop", "https://www.crossway.org/books/dark-clouds-deep-mercy-tpb/"],
     ["music", "Challies' Favorite Songs of 2023", "Spotify", "https://open.spotify.com/playlist/3bjWB2yqxipS3x8crkua2Y?si=b80943d68add4c07"],
     ["article", "Press-pulse: a novel therapeutic strategy for the metabolic management of cancer", "Thomas N. Seyfried", "https://nutritionandmetabolism.biomedcentral.com/articles/10.1186/s12986-017-0178-2"],
-    ["money", "Sei Mee Tea", "My Freen Tea source", "https://groundgreentea.com"],
+    ["money", "Green Tea", "Sei Mee Tea", "https://groundgreentea.com"],
     ["book", "Redemptive-Historical Bible Reading Plan", "ESV", "https://reasonabletheology.org/wp-content/uploads/Redemptive-Historical-Bible-Reading-Plan.pdf"],
 ]
 ---
@@ -16,7 +16,7 @@ items: [
         <div class="text-xl leading-relaxed">
             <div class="prose:dark prose-xl prose-white prose-a:text-yellow-500 hover:prose-a:text-zinc-500 hover:prose-a:border-b hover:prose-a:border-zinc-500">
                 <h1 class="not-prose font-serif text-7xl mt-1.5">Now</h1>
-                <p class="leading-normal text-zinc-300 my-3">My name is Tim and this what I am doing right <a href="https://nownownow.com/about">now</a>.</p>
+                <p class="leading-normal text-zinc-300 my-3">I'm Tim and this what I am up to right <a href="https://nownownow.com/about">now</a>.</p>
 
                 <p>Living  in Hyde Park, MA with my beautiful wife Kate, and 3 young, energetic boys Caleb (7), Nathan (5), and Matthew (3). Volunteering with <a href="https://provenanceprecisionmedicine.org">The Provenance Foundation</a>, <a href="https://www.patricesurley.com">Revitalized Health with Patrice</a> and <a href="https://drmaryjodimilia.com">Dr. Mary Jo Dimilla</a>. Check out their patient-centered work!</p>
             </div>
@@ -26,7 +26,7 @@ items: [
                     <h2 class="font-semibold text-2xl mt-10">Enjoying:</h2>
                     <ul class="mt-4">
                         @foreach($page->items as $item)
-                            <li class="inline-block mt-3">
+                            <li class="block mt-3">
                                 <a class="flex hover:text-yellow-500" href="{{ $item[3] }}">
                                     <span class="text-yellow-500">
                                         @include('_icons.'.$item[0])
